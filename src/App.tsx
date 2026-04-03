301db9bc107058d127fa580800f19c2bd122a0ae
Alucard Utarefson <boris.java@gmail.com>
2026-04-03 02:47:48 -0400

    fix: use HashRouter to fix routing on GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './i18n';
 import { Layout } from './components/Layout';
 import { HomePage } from './pages/HomePage';
