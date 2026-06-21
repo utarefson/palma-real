8b18bf1d6ab8c11fa5d21482ea038bcb8fd034e2
Alucard Utarefson <boris.java@gmail.com>
2026-06-21 01:48:48 -0400

    Disabling expenses page

--- src/pages/ExpensesPage.tsx
+++ src/pages/ExpensesPage.tsx
@@ -1,9 +1,17 @@
-import { ExpensesFeeCalculator } from '../components/ExpensesFeeCalculator';
+import { useTranslation } from 'react-i18next';
+import { PagePlaceholder } from '../components/PagePlaceholder';
+//import { ExpensesFeeCalculator } from '../components/ExpensesFeeCalculator';
 
 export function ExpensesPage() {
+  const { t } = useTranslation();
+
   return (
-    <div>
-      <ExpensesFeeCalculator />
-    </div>
+    <PagePlaceholder
+      title={t('pages.expenses')}
+      description={t('navbar.expenses')}
+    />
+    // <div>
+    //   <ExpensesFeeCalculator />
+    // </div>
   );
 }
